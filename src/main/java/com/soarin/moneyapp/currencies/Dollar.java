c14b38d63244486ef01c1ed4d021e57e93b6cd07
Alex Martin <amartin@excilys.com>
2019-05-01 00:02:43 +0200

    [CHAPTER 3] Equality for All

--- src/main/java/com/soarin/moneyapp/currencies/Dollar.java
+++ src/main/java/com/soarin/moneyapp/currencies/Dollar.java
@@ -11,4 +11,13 @@ public class Dollar {
   public Dollar times(int multiplier) {
     return new Dollar(amount * multiplier);
   }
+  
+  @Override
+  public boolean equals(Object object) {
+    if (!(object instanceof Dollar)){
+      return false;
+    }
+    Dollar dollar = (Dollar) object;
+    return this.amount == dollar.amount;
+  }
 }
